ada0feb4ab3fc7688f072233c52e0c805216660c
Sri Prasanna <k.sriprasanna@gmail.com>
2020-08-31 06:59:41 +0100

    adapting to local

--- bin/console_runner.js
+++ bin/console_runner.js
@@ -4,52 +4,52 @@ var jsonSchema = require('jsonschema');
 var validate = jsonSchema.validate;
 var colors = require('colors');
 var libpath = require('path'),
-	fs = require('fs');
+  fs = require('fs');
 
 require('pretty-error').start();
 
 function clean_dir(val, dir) {
-    v = val.split(',')
-    .forEach(function(d){
-        dir.push(d);
+  v = val.split(',')
+    .forEach(function(d) {
+      dir.push(d);
     });
-    return dir;
+  return dir;
 }
 
 program
-    .version('0.0.1')
-    .option('-e, --endpoint [url]', 'xAPI endpoint')
-    .option('-u, --authUser [string]', 'Basic Auth Username')
-    .option('-p, --authPassword [string]', 'Basic Auth Password')
-    .option('-a, --basicAuth', 'Enable Basic Auth')
-    .option('-o, --oAuth1', 'Enable oAuth 1')
-    .option('-c, --consumer_key [string]', 'oAuth 1 Consumer Key')
-    .option('-s, --consumer_secret [string]', 'oAuth 1 Consumer Secret')
-    .option('-r, --request_token_path [string]', 'Path to OAuth request token endpoint (relative to endpoint)')
-    .option('-t, --auth_token_path [string]', 'Path to OAuth authorization token endpoint (relative to endpoint)')
-    .option('-l, --authorization_path [string]', 'Path to OAuth user authorization endpoint (relative to endpoint)')
-    .option('-g, --grep [string]', 'Only run tests that match the given pattern')
-    .option('-b, --bail', 'Abort the battery if one test fails')
-    .option('-d, --directory [value]', 'Specific directories of tests (as a comma seperated list with no spaces)', clean_dir, ['v1_0_3'])
-    .option('-z, --errors', 'Results log of failing tests only')
-    .parse(process.argv);
+  .version('0.0.1')
+  .option('-e, --endpoint [url]', 'xAPI endpoint')
+  .option('-u, --authUser [string]', 'Basic Auth Username')
+  .option('-p, --authPassword [string]', 'Basic Auth Password')
+  .option('-a, --basicAuth', 'Enable Basic Auth')
+  .option('-o, --oAuth1', 'Enable oAuth 1')
+  .option('-c, --consumer_key [string]', 'oAuth 1 Consumer Key')
+  .option('-s, --consumer_secret [string]', 'oAuth 1 Consumer Secret')
+  .option('-r, --request_token_path [string]', 'Path to OAuth request token endpoint (relative to endpoint)')
+  .option('-t, --auth_token_path [string]', 'Path to OAuth authorization token endpoint (relative to endpoint)')
+  .option('-l, --authorization_path [string]', 'Path to OAuth user authorization endpoint (relative to endpoint)')
+  .option('-g, --grep [string]', 'Only run tests that match the given pattern')
+  .option('-b, --bail', 'Abort the battery if one test fails')
+  .option('-d, --directory [value]', 'Specific directories of tests (as a comma seperated list with no spaces)', clean_dir, ['v1_0_3'])
+  .option('-z, --errors', 'Results log of failing tests only')
+  .parse(process.argv);
 
 var options = {
-        endpoint: program.endpoint,
-        authUser: program.authUser,
-        authPass: program.authPassword,
-        basicAuth: program.basicAuth,
-        oAuth1: program.oAuth1,
-        consumer_key: program.consumer_key,
-        consumer_secret: program.consumer_secret,
-        request_token_path: program.request_token_path,
-        auth_token_path: program.auth_token_path,
-        authorization_path: program.authorization_path,
-        grep: program.grep,
-        bail: program.bail,
-        directory: program.directory,
-		errors: program.errors
-    }
+  endpoint: program.endpoint,
+  authUser: program.authUser,
+  authPass: program.authPassword,
+  basicAuth: program.basicAuth,
+  oAuth1: program.oAuth1,
+  consumer_key: program.consumer_key,
+  consumer_secret: program.consumer_secret,
+  request_token_path: program.request_token_path,
+  auth_token_path: program.auth_token_path,
+  authorization_path: program.authorization_path,
+  grep: program.grep,
+  bail: program.bail,
+  directory: program.directory,
+  errors: program.errors
+}
 
 /*
 var valid = validate(options, {
@@ -92,140 +92,137 @@ var testRunner = null;
 
 //catches ctrl+c event
 process.on('SIGINT', function() {
-    console.log(colors.white('Aborting tests'));
-	testRunner.cancel();
+  console.log(colors.white('Aborting tests'));
+  testRunner.cancel();
 });
 
 
 process.on('exit', function() {
-    console.log(colors.white('Closed'));
+  console.log(colors.white('Closed'));
 })
 
-function start(options)
-{
-    //These are already used to fetch the access token, and are not needed by the runer
-    delete options.request_token_path ;
-    delete options.auth_token_path ;
-    delete options.authorization_path ;
+function start(options) {
+  //These are already used to fetch the access token, and are not needed by the runer
+  delete options.request_token_path;
+  delete options.auth_token_path;
+  delete options.authorization_path;
 
-	testRunner = new TestRunner('console', null, options);
-    testRunner.start();
+  testRunner = new TestRunner('console', null, options);
+  testRunner.start();
 
-	var interval = setInterval(function(){
-		console.log(JSON.stringify(testRunner.summary));
-	}.bind(this), 2000);
+  var interval = setInterval(function() {
+    console.log(JSON.stringify(testRunner.summary));
+  }.bind(this), 2000);
 
-	testRunner.on('message', function(msg)
-	{
+  testRunner.on('message', function(msg) {
 
-		if(msg.action === 'log'){
-			console.log(msg.payload);
-		}
-		else if(msg.action === 'end')
-		{
-			clearInterval(interval);
-			console.log(JSON.stringify(testRunner.summary));
-			console.log(`Tests completed in ${testRunner.duration/1000} seconds`);
+    if (msg.action === 'log') {
+      console.log(msg.payload);
+    } else if (msg.action === 'end') {
+      clearInterval(interval);
+      console.log(JSON.stringify(testRunner.summary));
+      console.log(`Tests completed in ${testRunner.duration / 1000} seconds`);
 
-			function removeNulls (log)
-			{
-				var temp;
+      function removeNulls(log) {
+        var temp;
 
-				if (log && log.status === 'failed')
-				{
-					temp = {
-						title: log.title,
-						name: log.name,
-						requirement: log.requirement,
-						log:log.log,
-						status: log.status,
-						error: log.error
-					};
-					var t = log.tests.map(removeNulls);
-					if (t) temp.tests = t.filter(function(v){return v != undefined});
-				}
-				return temp;
-			}
+        if (log && log.status === 'failed') {
+          temp = {
+            title: log.title,
+            name: log.name,
+            requirement: log.requirement,
+            log: log.log,
+            status: log.status,
+            error: log.error
+          };
+          var t = log.tests.map(removeNulls);
+          if (t)
+            temp.tests = t.filter(function(v) {
+              return v != undefined
+            });
+        }
+        return temp;
+      }
 
-			// write log to file
-			var cleanLog = testRunner.getCleanRecord();
-			var output;
-			if (options.errors) {
-				var errOnly = {
-					name: cleanLog.name,
-					owner: cleanLog.owner,
-					flags: cleanLog.flags,
-					options: cleanLog.options,
-					rollupRule: cleanLog.rollupRule,
-					uuid: cleanLog.uuid,
-					startTime: cleanLog.startTime,
-					endTime: cleanLog.endTime,
-					duration: cleanLog.duration,
-					state: cleanLog.state,
-					summary: cleanLog.summary,
-					log: removeNulls(cleanLog.log)
-				};
-				output = JSON.stringify(errOnly, null, '    ');
-			} else {
-				output = JSON.stringify(cleanLog, null, '    ');
-			}
+      // write log to file
+      var cleanLog = testRunner.getCleanRecord();
+      var output;
+      if (options.errors) {
+        var errOnly = {
+          name: cleanLog.name,
+          owner: cleanLog.owner,
+          flags: cleanLog.flags,
+          options: cleanLog.options,
+          rollupRule: cleanLog.rollupRule,
+          uuid: cleanLog.uuid,
+          startTime: cleanLog.startTime,
+          endTime: cleanLog.endTime,
+          duration: cleanLog.duration,
+          state: cleanLog.state,
+          summary: cleanLog.summary,
+          log: removeNulls(cleanLog.log)
+        };
+        output = JSON.stringify(errOnly, null, '    ');
+      } else {
+        output = JSON.stringify(cleanLog, null, '    ');
+      }
 
-			var outDir = libpath.join(__dirname, '../logs');
+      var outDir = libpath.join(__dirname, '../logs');
 
-            // console.log(require("util").inspect(JSON.parse(JSON.stringify(cleanLog,function(k,v){if(k=="log" && v && v.constructor == String) return undefined; return v})),{depth:10}));
+      // console.log(require("util").inspect(JSON.parse(JSON.stringify(cleanLog,function(k,v){if(k=="log" && v && v.constructor == String) return undefined; return v})),{depth:10}));
 
-			fs.mkdir(outDir, 0o775, function(){
-				var outPath = libpath.join(outDir, testRunner.uuid+'.log');
-				fs.writeFile(outPath, output, (err, data) => {
-					if (err) {
-						console.log(err);
-						return process.exit(1);
-					}
-					console.log('Full run log written to', outPath);
-					return process.exit(testRunner.summary.failed);
-				});
-			});
-		}
-	});
+      fs.mkdir(outDir, 0o775, function() {
+        var outPath = libpath.join(outDir, 'tests.log');
+        fs.writeFile(outPath, output, (err, data) => {
+          if (err) {
+            console.log(err);
+            return process.exit(1);
+          }
+          console.log('Full run log written to', outPath);
+          return process.exit(testRunner.summary.failed);
+        });
+      });
+    }
+  });
 }
 
 if (!program.oAuth1)
-	start(options);
+  start(options);
 else {
 
 
-    var config = {};
-    config.consumer_key = options.consumer_key;
-    config.consumer_secret = options.consumer_secret;
+  var config = {};
+  config.consumer_key = options.consumer_key;
+  config.consumer_secret = options.consumer_secret;
 
-    //defaults for the ADL LRS
-    config.request_token_path = options.request_token_path || '/OAuth/initiate';
-    config.auth_token_path = options.auth_token_path ||'/OAuth/token';
-    config.authorization_path = options.authorization_path || "/../accounts/login?next=/XAPI/OAuth/authorize";
+  //defaults for the ADL LRS
+  config.request_token_path = options.request_token_path || '/OAuth/initiate';
+  config.auth_token_path = options.auth_token_path || '/OAuth/token';
+  config.authorization_path = options.authorization_path || "/../accounts/login?next=/XAPI/OAuth/authorize";
 
-    config.endpoint = options.endpoint;
-    require("./OAuth.js").auth(config, function(err, oAuth) {
+  config.endpoint = options.endpoint;
+  require("./OAuth.js").auth(config, function(err, oAuth) {
 
-        if (err) {
-            console.log(err);
-            process.exit();
-        }
-        options.consumer_key = options.consumer_key;
-        options.consumer_secret = options.consumer_secret;
-        options.token = oAuth.token;
-        options.token_secret = oAuth.token_secret;
-        options.verifier = oAuth.verifier;
+    if (err) {
+      console.log(err);
+      process.exit();
+    }
+    options.consumer_key = options.consumer_key;
+    options.consumer_secret = options.consumer_secret;
+    options.token = oAuth.token;
+    options.token_secret = oAuth.token_secret;
+    options.verifier = oAuth.verifier;
 
 
-        var OAUTH = {
+    var OAUTH = {
 
-            consumer_key: options.consumer_key,
-            consumer_secret: options.consumer_secret,
-            token: options.token,
-            token_secret: options.token_secret,
-            verifier: options.verifier
-        }
+      consumer_key: options.consumer_key,
+      consumer_secret: options.consumer_secret,
+      token: options.token,
+      token_secret: options.token_secret,
+      verifier: options.verifier
+    }
 
-		start(options);
-    });
+    start(options);
+  });
 }
--- bin/lrs-test.js
+++ bin/lrs-test.js
@@ -3,192 +3,195 @@
  * Description : This is the command line interface for running the lrs conformance test suite.
  *
  */
-(function (process, require, program, exit, packageJson, Q, Joi, fs, path, Mocha) {
-    'use strict';
+(function(process, require, program, exit, packageJson, Q, Joi, fs, path, Mocha) {
+  'use strict';
 
-    function processMessageReporter(p) {
-        return function(runner) {
-            runner.on('test', function(test) {
-                p.postMessage("test start", test.title);
-            })
-            runner.on('test end', function(test) {
-                p.postMessage("test end", test.title);
-            })
-            runner.on('pass', function(test) {
-                p.postMessage("test pass", test.title);
-            })
-            runner.on('fail', function(test, err) {
-                p.postMessage("test fail", {title:test.title,message:err.toString()});
-            })
-            runner.on('end', function() {
-                p.postMessage("end", 'All done');
-            });
-            runner.on('pending', function(test) {
-                p.postMessage("pending", test.title);
-            });
-            runner.on('start', function() {
-                p.postMessage("start", runner.total);
-            });
-            runner.on('suite', function(suite) {
-                p.postMessage("suite start", suite.title);
-            });
-            runner.on('suite end', function(suite) {
-                p.postMessage('suite end', suite.title);
-            });
-        }
-    }
-
-    function runTests(_options) {
-        var optionsValidator = Joi.object({
-            directory: Joi.array().items(Joi.string().required()),
-            /* See [RFC-3986](http://tools.ietf.org/html/rfc3986#page-17) */
-            endpoint: Joi.string().regex(/^[a-zA-Z][a-zA-Z0-9+\.-]*:.+/, 'URI').required(),
-            grep: Joi.string(),
-            optional: Joi.array().items(Joi.string().required()),
-            basicAuth: Joi.any(true, false),
-            oAuth1: Joi.any(true, false),
-            authUser: Joi.string().when('basicAuth', {
-                is: 'true',
-                then: Joi.required()
-            }),
-            authPass: Joi.string().when('basicAuth', {
-                is: 'true',
-                then: Joi.required()
-            }),
-            consumer_key: Joi.string().when('oAuth1', {
-                is: 'true',
-                then: Joi.required()
-            }),
-            consumer_secret: Joi.string().when('oAuth1', {
-                is: 'true',
-                then: Joi.required()
-            }),
-            token: Joi.string().when('oAuth1', {
-                is: 'true',
-                then: Joi.required()
-            }),
-            token_secret: Joi.string().when('oAuth1', {
-                is: 'true',
-                then: Joi.required()
-            }),
-            verifier: Joi.string().when('oAuth1', {
-                is: 'true',
-                then: Joi.required()
-            }),
-            reporter: Joi.string().regex(/^((dot)|(spec)|(nyan)|(tap)|(List)|(progress)|(min)|(doc))$/).default('nyan'),
-            bail: Joi.boolean(),
-            errors: Joi.boolean()
-        }).unknown(false);
-
-        var validOptions = Joi.validate(_options, optionsValidator);
-        if (validOptions.error) {
-            process.postMessage("log", "Options not valid " + validOptions.error);
-            process.exit();
-        }
-
-        var DIRECTORY = ['v1_0_3'];
-        var options = {
-            directory: _options.directory || DIRECTORY,
-            endpoint: _options.endpoint,
-            basicAuth: _options.basicAuth,
-            authUser: _options.authUser,
-            authPass: _options.authPass,
-            reporter: _options.reporter,
-            grep: _options.grep,
-            optional: _options.optional,
-            bail: _options.bail,
-            consumer_key: _options.consumer_key,
-            consumer_secret: _options.consumer_secret,
-            token: _options.token,
-            token_secret: _options.token_secret,
-            verifier: _options.verifier,
-            oAuth1: _options.oAuth1,
-            errors: _options.errors
-        };
-
-        var grep;
-        if (options.grep){
-          grep = new RegExp(options.grep);
-        }
-
-        var mocha = new Mocha({
-            uii: 'bdd',
-            reporter: processMessageReporter(process),
-            timeout: '15000',
-            grep: grep,
-            bail: options.bail
+  function processMessageReporter(p) {
+    return function(runner) {
+      runner.on('test', function(test) {
+        p.postMessage("test start", test.title);
+      })
+      runner.on('test end', function(test) {
+        p.postMessage("test end", test.title);
+      })
+      runner.on('pass', function(test) {
+        p.postMessage("test pass", test.title);
+      })
+      runner.on('fail', function(test, err) {
+        p.postMessage("test fail", {
+          title: test.title,
+          message: err.toString()
         });
+      })
+      runner.on('end', function() {
+        p.postMessage("end", 'All done');
+      });
+      runner.on('pending', function(test) {
+        p.postMessage("pending", test.title);
+      });
+      runner.on('start', function() {
+        p.postMessage("start", runner.total);
+      });
+      runner.on('suite', function(suite) {
+        p.postMessage("suite start", suite.title);
+      });
+      runner.on('suite end', function(suite) {
+        p.postMessage('suite end', suite.title);
+      });
+    }
+  }
 
-        console.log("Grep is " + grep);
-        process.env.DIRECTORY = options.directory[0];
+  function runTests(_options) {
+    var optionsValidator = Joi.object({
+      directory: Joi.array().items(Joi.string().required()),
+      /* See [RFC-3986](http://tools.ietf.org/html/rfc3986#page-17) */
+      endpoint: Joi.string().regex(/^[a-zA-Z][a-zA-Z0-9+\.-]*:.+/, 'URI').required(),
+      grep: Joi.string(),
+      optional: Joi.array().items(Joi.string().required()),
+      basicAuth: Joi.any(true, false),
+      oAuth1: Joi.any(true, false),
+      authUser: Joi.string().when('basicAuth', {
+        is: 'true',
+        then: Joi.required()
+      }),
+      authPass: Joi.string().when('basicAuth', {
+        is: 'true',
+        then: Joi.required()
+      }),
+      consumer_key: Joi.string().when('oAuth1', {
+        is: 'true',
+        then: Joi.required()
+      }),
+      consumer_secret: Joi.string().when('oAuth1', {
+        is: 'true',
+        then: Joi.required()
+      }),
+      token: Joi.string().when('oAuth1', {
+        is: 'true',
+        then: Joi.required()
+      }),
+      token_secret: Joi.string().when('oAuth1', {
+        is: 'true',
+        then: Joi.required()
+      }),
+      verifier: Joi.string().when('oAuth1', {
+        is: 'true',
+        then: Joi.required()
+      }),
+      reporter: Joi.string().regex(/^((dot)|(spec)|(nyan)|(tap)|(List)|(progress)|(min)|(doc))$/).default('nyan'),
+      bail: Joi.boolean(),
+      errors: Joi.boolean()
+    }).unknown(false);
 
-        //adds optional tests to the front in ascending order
-        if (options.optional){
-          options.optional.reverse().forEach(function(dir) {
-              options.directory.unshift(dir);
-          });
-        }
-
-        console.log("directory is ", options.directory);
-
-
-        process.env.LRS_ENDPOINT = options.endpoint;
-        process.env.BASIC_AUTH_ENABLED = options.basicAuth;
-        process.env.BASIC_AUTH_USER = options.authUser;
-        process.env.BASIC_AUTH_PASSWORD = options.authPass;
-        process.env.OAUTH1_ENABLED = options.oAuth1;
-
-
-        if(options.oAuth1)
-        {
-           // console.log("USING OAUTH");
-            global.OAUTH = {
-
-                consumer_key: _options.consumer_key,
-                consumer_secret: _options.consumer_secret,
-                token: _options.token,
-                token_secret: _options.token_secret,
-                verifier: _options.verifier
-            }
-        }
-
-        options.directory.forEach(function(dir) {
-            //process.postMessage("log", (JSON.stringify(global.OAUTH)));
-            var testDirectory = __dirname + '/../test/' + dir;
-            fs.readdirSync(testDirectory).filter(function(file) {
-                return file.substr(-3) === '.js';
-            }).forEach(function(file) {
-                mocha.addFile(
-                    path.join(testDirectory, file)
-                );
-            });
-        });
-
-        mocha.run(function(failures) {
-            if (failures) {} else {}
-
-            process.postMessage("log", "Test Suite Complete");
-            process.exit();
-        })
+    var validOptions = Joi.validate(_options, optionsValidator);
+    if (validOptions.error) {
+      process.postMessage("log", "Options not valid " + validOptions.error);
+      process.exit();
     }
 
-    function hookupIPC() {
-        process.postMessage = function(action, payload) {
-            process.send({
-                action: action,
-                payload: payload
-            })
-        }
-        process.on('message', function(message) {
-            if (message.action == "ping") {
-                process.postMessage("log", "pong");
-            }
-            else if (message.action == "runTests") {
-                process.postMessage("log", "runTests starting");
-                runTests(message.payload);
-            }
-        })
-        process.postMessage("ready");
+    var DIRECTORY = ['v1_0_3'];
+    var options = {
+      directory: _options.directory || DIRECTORY,
+      endpoint: _options.endpoint,
+      basicAuth: _options.basicAuth,
+      authUser: _options.authUser,
+      authPass: _options.authPass,
+      reporter: _options.reporter,
+      grep: _options.grep,
+      optional: _options.optional,
+      bail: _options.bail,
+      consumer_key: _options.consumer_key,
+      consumer_secret: _options.consumer_secret,
+      token: _options.token,
+      token_secret: _options.token_secret,
+      verifier: _options.verifier,
+      oAuth1: _options.oAuth1,
+      errors: _options.errors
+    };
+
+    var grep;
+    if (options.grep) {
+      grep = new RegExp(options.grep);
     }
-    hookupIPC();
+
+    var mocha = new Mocha({
+      uii: 'bdd',
+      reporter: processMessageReporter(process),
+      timeout: '15000',
+      grep: grep,
+      bail: options.bail
+    });
+
+    console.log("Grep is " + grep);
+    process.env.DIRECTORY = options.directory[0];
+
+    //adds optional tests to the front in ascending order
+    if (options.optional) {
+      options.optional.reverse().forEach(function(dir) {
+        options.directory.unshift(dir);
+      });
+    }
+
+    console.log("directory is ", options.directory);
+
+
+    process.env.LRS_ENDPOINT = options.endpoint;
+    process.env.BASIC_AUTH_ENABLED = options.basicAuth;
+    process.env.BASIC_AUTH_USER = options.authUser;
+    process.env.BASIC_AUTH_PASSWORD = options.authPass;
+    process.env.OAUTH1_ENABLED = options.oAuth1;
+
+
+    if (options.oAuth1) {
+      // console.log("USING OAUTH");
+      global.OAUTH = {
+
+        consumer_key: _options.consumer_key,
+        consumer_secret: _options.consumer_secret,
+        token: _options.token,
+        token_secret: _options.token_secret,
+        verifier: _options.verifier
+      }
+    }
+
+    options.directory.forEach(function(dir) {
+      //process.postMessage("log", (JSON.stringify(global.OAUTH)));
+      var testDirectory = __dirname + '/../test/' + dir;
+      fs.readdirSync(testDirectory).filter(function(file) {
+        return file.substr(-3) === '.js';
+      }).forEach(function(file) {
+        mocha.addFile(
+          path.join(testDirectory, file)
+        );
+      });
+    });
+
+    mocha.run(function(failures) {
+      if (failures) {
+      } else {
+      }
+
+      process.postMessage("log", "Test Suite Complete");
+      process.exit();
+    })
+  }
+
+  function hookupIPC() {
+    process.postMessage = function(action, payload) {
+      process.send({
+        action: action,
+        payload: payload
+      })
+    }
+    process.on('message', function(message) {
+      if (message.action == "ping") {
+        process.postMessage("log", "pong");
+      } else if (message.action == "runTests") {
+        process.postMessage("log", "runTests starting");
+        runTests(message.payload);
+      }
+    })
+    process.postMessage("ready");
+  }
+  hookupIPC();
 }(process, require, require('commander'), require('exit'), require('../package.json'), require('q'), require('joi'), require('fs'), require('path'), require('mocha')));
